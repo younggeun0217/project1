1f41da0fde79c592cec66bb309157de454a2c0f8
publicminsu <publicminsu@naver.com>
2022-05-02 18:50:46 +0900

    app 주석처리

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
+    <div></div>
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +21,7 @@ function App() {
         </a>
       </header>
     </div>
+    */
   );
 }
 
