e8a30ebb9cc37f820a916d097f1248aa71a36e3e
publicminsu <publicminsu@naver.com>
2022-05-03 15:53:13 +0900

    타임탭 추가, css겹치는 문제 해결을 위한 css 모듈 사용

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,4 @@
-import "../css/MenuTab.css";
+import styles from "../css/MenuTab.module.css";
 export default function Header() {
-  return <header className="header">가짜뉴스판독기</header>;
+  return <header className={styles.header}>가짜뉴스판독기</header>;
 }
