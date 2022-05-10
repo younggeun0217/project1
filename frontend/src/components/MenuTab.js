6c5779a4fb01cc3049efc99e01af386916570d1a
publicminsu <publicminsu@naver.com>
2022-05-10 17:13:12 +0900

    css문제 해결

--- frontend/src/components/MenuTab.js
+++ frontend/src/components/MenuTab.js
@@ -39,7 +39,7 @@ export default function MenuTab() {
           return section.title;
         })}
       </ul>
-      <div>{menuArr[useIndex].content}</div>
+      {menuArr[useIndex].content}
     </>
   );
 }
